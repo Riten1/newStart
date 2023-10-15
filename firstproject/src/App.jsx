398ae81ccf162d6114c3ba773f8dec7d9df9abd5
Riten1 <122423532+Riten1@users.noreply.github.com>
2023-10-15 09:58:23 +0545

    'updates'

--- firstproject/src/App.jsx
+++ firstproject/src/App.jsx
@@ -1,11 +1,27 @@
-
-import './App.css'
+import { useState } from "react"
 
 function App() {
+let [count, setCount] = useState(0)
+
+function add(){
+  setCount(count + 1)
+}
+
+function sub(){
+  if (count == 0){
+    setCount(count = 0)
+  }else{
+  setCount(count - 1)}
+}
 
   return (
-    <h1>Riten Gurung</h1>
-  )
+    <div>
+      
+      <h1>{count} Riten Gurung </h1>
+      <button onClick={add}>+</button>
+      <button onClick={sub}>-</button>
+    </div>
+  );
 }
 
 export default App
